41561cae0bb99ad58bc41e1ddd98a5d826b36e78
savethewaves <chrislei@gmail.com>
2011-04-19 15:51:08 -0700

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 group :test do
   gem 'rspec', '2.5.0'
   gem 'webrat', '0.7.1'
+  gem 'spork' , '0.9.0.rc4'
 end
 
 # Bundle edge Rails instead:
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+  end
+  
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title", 
+      :content => "Ruby on Rails Tutorial Sample App | Home")
   end
 
   describe "GET 'contact'" do
@@ -15,5 +22,25 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title", 
+      :content => "Ruby on Rails Tutorial Sample App | Contact")
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+  it "should have the right title" do
+        get 'about'
+        response.should have_selector("title",
+                          :content =>
+                            "Ruby on Rails Tutorial Sample App | About")
+  end
+  
 end
