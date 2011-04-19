32fc493e508d8abd811324a099cb8bb53edaed08
savethewaves <chrislei@gmail.com>
2011-04-19 16:02:57 -0700

    Added help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -43,4 +43,18 @@ describe PagesController do
                             "Ruby on Rails Tutorial Sample App | About")
   end
   
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
+  
+  it "should have the right title" do
+        get 'help'
+        response.should have_selector("title",
+                          :content =>
+                            "Ruby on Rails Tutorial Sample App | Help")
+  end
+  
 end
