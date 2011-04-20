974069b5ec09f2b716465ecc90db13fa2ad47fd6
savethewaves <chrislei@gmail.com>
2011-04-19 17:21:29 -0700

    added consolidated base_title string to pages controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    # Define @base_title here.
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -13,7 +18,7 @@ describe PagesController do
   it "should have the right title" do
     get 'home'
     response.should have_selector("title", 
-      :content => "Ruby on Rails Tutorial Sample App | Home")
+      :content => @base_title + " | Home")
   end
 
   describe "GET 'contact'" do
@@ -26,7 +31,7 @@ describe PagesController do
   it "should have the right title" do
     get 'contact'
     response.should have_selector("title", 
-      :content => "Ruby on Rails Tutorial Sample App | Contact")
+      :content => @base_title + " | Contact")
   end
   
   describe "GET 'about'" do
@@ -39,8 +44,7 @@ describe PagesController do
   it "should have the right title" do
         get 'about'
         response.should have_selector("title",
-                          :content =>
-                            "Ruby on Rails Tutorial Sample App | About")
+                          :content => @base_title + " | About")
   end
   
   describe "GET 'help'" do
@@ -53,8 +57,7 @@ describe PagesController do
   it "should have the right title" do
         get 'help'
         response.should have_selector("title",
-                          :content =>
-                            "Ruby on Rails Tutorial Sample App | Help")
+                          :content => @base_title + " | Help")
   end
   
 end
